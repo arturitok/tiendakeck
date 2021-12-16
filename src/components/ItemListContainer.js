9fbc4183bea5458163f519a6b64aaea96d4b26a9
arturitok <arturokeck@gmail.com>
2021-12-16 05:14:04 -0300

    Desafío: Contador con botón // Mostrar unidades en el padre

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,17 @@
 import "../estilos.css";
+import { useState } from "react";
 import ItemCount from "./ItemCount";
+
 const ItemListContainer = ({ greeting }) => {
+  const [cantidad, setQty] =useState(0)
   const onAdd = (cantidad) => {
     console.log(`Agregando ${cantidad} artículos al carrito`);
+    setQty(cantidad)
   };
   return (
   <div> 
     <h2 className="product-list">{greeting}</h2>
+    <p>Elegiste: {cantidad} unidades</p>
     <ItemCount  stock={5} initial={1} onAdd={onAdd}/>
   </div>);
 };
