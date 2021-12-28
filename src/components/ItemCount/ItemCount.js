783acfa66c27a08eba62d957969a781e6199488b
arturitok <arturokeck@gmail.com>
2021-12-28 20:27:53 -0300

    Desafío: Sincronizar counter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,34 +1,38 @@
 import React, { useState, useEffect } from "react";
-import "../ItemCount/ItemCount.css"
+import "../ItemCount/ItemCount.css";
 
-function ItemCount( {stock, onAdd, initial} ) {
-    const [count, setCount] = useState(initial)
+function ItemCount({ stock, onAdd, initial }) {
+  const [count, setCount] = useState(initial);
 
-    const incrementar = () => {
-        if(count < stock) {
-            setCount(count+1)
-        }
+  const incrementar = () => {
+    if (count < stock) {
+      setCount(count + 1);
     }
+  };
 
-    const decrementar = () => {
-        if(count > initial) {
-            setCount(count-1)
-        }
+  const decrementar = () => {
+    if (count > initial) {
+      setCount(count - 1);
     }
+  };
 
-    useEffect(() => {
-        onAdd(count)
-    }, [count])
+  useEffect(() => {
+    onAdd(count);
+  }, [count]);
 
-
-    return (
-        <div className="contenedor">  
-        <div className="contador">
-            <button className="botones" onClick={decrementar}>-</button>
-            <div className="cantidad">{count}</div>
-            <button className="botones" onClick={incrementar}>+</button>
-        </div>
-    </div>    )
+  return (
+    <div className="contenedor">
+      <div className="contador">
+        <button className="botones" onClick={decrementar}>
+          -
+        </button>
+        <div className="cantidad">{count}</div>
+        <button className="botones" onClick={incrementar}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default ItemCount
+export default ItemCount;
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,45 +1,54 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
-import "../ItemDetail/ItemDetail.css"
-import ItemCount from "../ItemCount/ItemCount"
+import { Link } from "react-router-dom";
+import "../ItemDetail/ItemDetail.css";
+import ItemCount from "../ItemCount/ItemCount";
 
-function ItemDetail( {title, price, pictureUrl, description} ) {
+function ItemDetail({ title, price, pictureUrl, description }) {
+  const [itemCountVisible, setItemCountVisible] = useState(true);
+  const [buyButtonVisibility, setBuyButtonVisibility] = useState(true);
+  const [itemsCount, setItemsCount] = useState(0);
 
-    const [itemCountVisible, setItemCountVisible] = useState(true)
-    const [buyButtonVisibility, setBuyButtonVisibility] = useState(true)
-    const [itemsCount, setItemsCount] = useState(0)
+  const onAdd = (count) => {
+    setItemsCount(count);
+  };
 
-    const onAdd = (count) => {
-        setItemsCount(count)
-    }
+  const onAddToCart = () => {
+    setItemCountVisible(false);
+    setBuyButtonVisibility(false);
+  };
 
-    const onAddToCart = () => {
-        setItemCountVisible(false)
-        setBuyButtonVisibility(false)
-    }
+  return (
+    <div className="div">
+      <div className="container-detail">
+        <div
+          style={{ backgroundImage: `url(${pictureUrl})` }}
+          className="img-detail"
+        ></div>
+        <div className="informacion-detail">
+          <div className="title-detail">{title}</div>
+          <div className="description-detail">{description}</div>
+          <div className="price-detail">{price}</div>
 
-    return(
-        <div className="div">
-            <div className="container-detail">
-                <div style={{backgroundImage: `url(${pictureUrl})`}} className="img-detail"></div>   
-                <div className="informacion-detail">    
-                    <div className="title-detail">{title}</div>
-                    <div className="description-detail">{description}</div>
-                    <div className="price-detail">{price}</div>
-                    
-                    {itemCountVisible && <ItemCount stock={10} initial={0} onAdd={onAdd}/>}
-                    {buyButtonVisibility && <button onClick={onAddToCart} className="cart-button">Agregar al carrito</button>}
-                    {!buyButtonVisibility && 
-                    <div className="contenedor-terminar-compra">
-                        <h1>x{itemsCount} Unidades</h1>
-                        <Link to="/cart">
-                            <button className="buy-button">Terminar compra</button>
-                        </Link>
-                    </div>} 
-                </div>
+          {itemCountVisible && (
+            <ItemCount stock={10} initial={0} onAdd={onAdd} />
+          )}
+          {buyButtonVisibility && (
+            <button onClick={onAddToCart} className="cart-button">
+              Agregar al carrito
+            </button>
+          )}
+          {!buyButtonVisibility && (
+            <div className="contenedor-terminar-compra">
+              <h1>x{itemsCount} Unidades</h1>
+              <Link to="/cart">
+                <button className="buy-button">Terminar compra</button>
+              </Link>
             </div>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ItemDetail
+export default ItemDetail;
